55b6fae9a0017ded277de294c709dc85e7ece69e
ilvdrskn <99636754+ilvdrskn@users.noreply.github.com>
2023-03-17 01:15:02 +0300

    made desktop version

---
+++ logic.js
@@ -0,0 +1,17 @@
+fetch('./data.json').then(item => item.json()).then(
+    data => {
+        for (let i = 0; i < data.length; i++) {
+            document.getElementsByClassName(`${data[i].day}`)[0].onmouseover = function() {
+                document.getElementsByClassName('overlay')[i].innerHTML = "$" + data[i].amount;
+            }
+
+            document.getElementsByClassName(`${data[i].day}`)[0].onmouseout = function() {
+                document.getElementsByClassName('overlay')[i].innerHTML = '';
+            }
+
+        }
+    }
+);
+
+
+
