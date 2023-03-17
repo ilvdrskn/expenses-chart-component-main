beb1db8a058d176c89b7fe7031ad2043e9a39d0b
ilvdrskn <99636754+ilvdrskn@users.noreply.github.com>
2023-03-17 22:02:57 +0300

    finished challenge

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 fetch('./data.json').then(item => item.json()).then(
     data => {
         for (let i = 0; i < data.length; i++) {
+            document.getElementsByClassName(`${data[i].day}`)[0].style.height = `${data[i].amount * 2}px`;
             document.getElementsByClassName(`${data[i].day}`)[0].onmouseover = function() {
                 document.getElementsByClassName('overlay')[i].innerHTML = "$" + data[i].amount;
             }
